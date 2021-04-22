3d3d4ef3b21134449ea5993d86545097a1c24f37
abhishekashyap <29458374+abhishekashyap@users.noreply.github.com>
2021-04-23 02:33:00 +0530

    Added test case for empty string, single vowel and multiple vowels

--- src/test/java/co/interleap/courses/tdd/VowelCounterTest.java
+++ src/test/java/co/interleap/courses/tdd/VowelCounterTest.java
@@ -1,9 +1,25 @@
 package co.interleap.courses.tdd;
 
+import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class VowelCounterTest {
 
     @Test
-    public void dummy(){}
+    public void testEmptyString(){
+     VowelCounter aVowelCounter = new VowelCounter();
+     assertEquals(0, aVowelCounter.count(""));
+    }
+
+    @Test
+    public void testSingleVowelNoConsonants(){
+        VowelCounter aVowelCounter = new VowelCounter();
+        assertEquals(1, aVowelCounter.count("e"));
+    }
+
+    @Test
+    public void testMultipleVowelsNoConsonants(){
+        VowelCounter aVowelCounter = new VowelCounter();
+        assertEquals(9, aVowelCounter.count("aaeiiioou"));
+    }
 }
